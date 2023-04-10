94815bcc785994368b466a16cc66713347f8abf5
David <davidmollera@hotmail.com>
2023-04-10 16:22:41 -0500

    added some general html code to the app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="info__container">
+            <div className="info__personal"></div>
+            <div className="info__links"></div>
+            <div className="info__footer"></div>
+          </div>
+          <div className="projects__container"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
