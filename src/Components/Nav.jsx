b604d17281eb395f5a9407480a120c49eb264829
David <davidmollera@hotmail.com>
2023-04-11 10:08:05 -0500

    created a nav component

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="info__container">
+      <Personal />
+      <Links />
+      <Footer />
+    </div>
+  );
+};
+
+export default Nav;
