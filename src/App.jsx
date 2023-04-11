f89662da269b9ad3c602b61acf855764ec624312
David Moller <davidmollera@hotmail.com>
2023-04-11 13:34:35 -0500

    created the route to the project info page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import Landing from "./Pages/Landing";
+import Projectinfo from "./Pages/Projectinfo";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing/>}/>
+        <Route path="/project/:id" element={<Projectinfo/>}/>
       </Routes>
     </Router>
   );
---
+++ src/Pages/Projectinfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projectinfo = () => {
+    return (
+        <div>
+            test
+        </div>
+    );
+}
+
+export default Projectinfo;
