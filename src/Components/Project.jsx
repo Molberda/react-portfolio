2602a25b03df2e4a2c9e826a2164efe5871bdfa7
David <davidmollera@hotmail.com>
2023-04-10 23:25:27 -0500

    created css for the project jsx

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Project = () => {
-    return (
-        <div className='project__container'>
-            <figure className="project__wrapper">
-                test
-            </figure>
-        </div>
-    );
-}
+  return (
+    <div className="project__container">
+      <figure className="project__wrapper">
+        <img
+          src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"
+          alt=""
+          className="project__img"
+        />
+        <span className="project__background">
+            <h1 className="project__title">title</h1>
+        </span>
+      </figure>
+    </div>
+  );
+};
 
 export default Project;
