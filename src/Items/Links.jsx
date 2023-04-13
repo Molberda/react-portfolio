a5d5535173ccaec13c88529480e75c4807338609
David Moller <davidmollera@hotmail.com>
2023-04-12 22:27:33 -0500

    updated every btn in info links to use mui btns

--- src/Items/Links.jsx
+++ src/Items/Links.jsx
@@ -6,21 +6,20 @@ const Links = () => {
         <div className="info__links">
               <ul className="links__list">
                 <li className="links__item">
-                  <button className="link click">
+                <Button size='small' className="link click">
                     About Me
-                  </button>
+                  </Button>
                 </li>
                 <li className="links__item">
-                  <button className="link click">
+                <Button size='small' className="link click">
                     Dark Mode
-                  </button>
+                  </Button>
                 </li>
                 <li className="links__item">
                   <Button size='small' className="link click">
                     Contact Me
                   </Button>
                 </li>
-                <Button size='small' className="link click" variant="text">Text</Button>
               </ul>
             </div>
     );
