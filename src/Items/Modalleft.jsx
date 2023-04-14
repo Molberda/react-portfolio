8b7627feb186a324ffc157022bee1d4e44adbb4c
David Moller <davidmollera@hotmail.com>
2023-04-14 12:26:50 -0500

    adding information to the modalleft.jsx

--- src/Items/Modalleft.jsx
+++ src/Items/Modalleft.jsx
@@ -7,7 +7,16 @@ const Modalleft = () => {
   }
   return (
     <div className="modalleft">
-      <button onClick={toggleModal}>close</button>
+      <div className="modal__info">
+        <p className="modal__title">
+          Hello, I'm <span className="highlight">David Moller</span> and I'm a
+          22 year old <span className="highlight">frontend developer</span> with
+          a strong desire to grow, improve and acquire knowledge. <br />
+          I'm currently working with Html, Css, JavaScript and React as a
+          freelance developer, helping young entrepreneurs build their dream
+          website{" "}
+        </p>
+      </div>
     </div>
   );
 };
