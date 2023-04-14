3bb71dda01c13dfe854950c4a48bb1e21c268f0b
David Moller <davidmollera@hotmail.com>
2023-04-14 12:59:26 -0500

    adding more css to modal left

--- src/Items/Modalleft.jsx
+++ src/Items/Modalleft.jsx
@@ -8,10 +8,13 @@ const Modalleft = () => {
   return (
     <div className="modalleft">
       <div className="modal__info">
-        <p className="modal__title">
-          Hello, I'm <span className="highlight">David Moller</span> and I'm a
-          22 year old <span className="highlight">frontend developer</span> with
-          a strong desire to grow, improve and acquire knowledge. <br />
+        <h1 className="modal__title">
+          Hello, I'm <span className="highlight">David Moller</span>
+        </h1>
+        <p className="modal__text">
+          And I'm a 22 year old{" "}
+          <span className="highlight">frontend developer</span> with a strong
+          desire to grow, improve and acquire knowledge. <br />
           I'm currently working with Html, Css, JavaScript and React as a
           freelance developer, helping young entrepreneurs build their dream
           website{" "}
