18131339ffceff7e333c2bda22d02db888a2a880
David Moller <davidmollera@hotmail.com>
2023-04-15 19:18:06 -0500

    used display flex for the language wrapper

--- src/Items/Modalleft.jsx
+++ src/Items/Modalleft.jsx
@@ -23,12 +23,13 @@ const Modalleft = () => {
           <div className="modal__languages--container">
             <ul className="languages__list">
               <li className="language__item">
-                <figure className="language__wrapper click">
+                <figure className="language__wrapper">
                   <img
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png"
                     alt=""
                     className="language__img"
                   />
+                  <p className="language__name">css</p>
                 </figure>
               </li>
             </ul>
