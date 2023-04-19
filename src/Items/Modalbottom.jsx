2e387f0381e635cbab0c0a53ee67479018eedbda
David Moller <davidmollera@hotmail.com>
2023-04-19 18:39:20 -0500

    modified the general background color of the page and added html for the modal bottom

--- src/Items/Modalbottom.jsx
+++ src/Items/Modalbottom.jsx
@@ -2,7 +2,28 @@ import React from "react";
 import "../Components/Modal.css";
 
 const Modalbottom = () => {
-  return <div className="modalbottom">test</div>;
+  return (
+    <div className="modalbottom">
+      <div className="modal__info">
+        <h1 className="modal__title">
+          Hello, I'm <span className="highlight">David Moller</span>
+        </h1>
+        <div className="modal__info--container">
+          <p className="modal__text">
+            And I'm a 22 year old{" "}
+            <span className="highlight">frontend developer</span> with a strong
+            desire to grow, improve and acquire knowledge. <br />
+            <br />
+            I'm currently working with Html, Css, JavaScript and React as a
+            freelance developer, helping young entrepreneurs build their dream
+            website{" "}
+          </p>
+          <div className="modal__languages--container">
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Modalbottom;
