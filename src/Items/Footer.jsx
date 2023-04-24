44ce02e7a736d997687e8b7c62582ba9ab45bf96
David Moller <davidmollera@hotmail.com>
2023-04-24 11:36:25 -0500

    created a contrast on function and used the contrast btn to call it

--- src/Items/Footer.jsx
+++ src/Items/Footer.jsx
@@ -10,33 +10,53 @@ import { IconButton } from "@mui/material";
 import React from "react";
 
 const Footer = () => {
+  function contrastOn(){
+    console.log('contrast')
+  }
+
   return (
     <div className="info__footer">
       <ul className="footer__list">
         <li className="footer__item whabtn--hover click">
           <IconButton className="icon__btn">
-            <a href="https://wa.me/573163402080" target="_blank" className="icon__link">
+            <a
+              href="https://wa.me/573163402080"
+              target="_blank"
+              className="icon__link"
+            >
               <WhatsApp className="whabtn" />
             </a>
           </IconButton>
         </li>
         <li className="footer__item mailbtn--hover click">
           <IconButton className="icon__btn">
-            <a href="mailto:davidmollera@hotmail.com" target="_blank" className="icon__link">
+            <a
+              href="mailto:davidmollera@hotmail.com"
+              target="_blank"
+              className="icon__link"
+            >
               <Mail className="mailbtn" />
             </a>
           </IconButton>
         </li>
         <li className="footer__item gitbtn--hover click">
           <IconButton className="icon__btn ">
-            <a href="https://github.com/Molberda" target="_blank" className="icon__link">
+            <a
+              href="https://github.com/Molberda"
+              target="_blank"
+              className="icon__link"
+            >
               <GitHub className="gitbtn" />
             </a>
           </IconButton>
         </li>
         <li className="footer__item linkedbtn--hover click">
           <IconButton className="icon__btn">
-            <a href="https://www.linkedin.com/in/david-moller-2a9512255/" target="_blank" className="icon__link">
+            <a
+              href="https://www.linkedin.com/in/david-moller-2a9512255/"
+              target="_blank"
+              className="icon__link"
+            >
               <LinkedIn className="linkedbtn" />
             </a>
           </IconButton>
@@ -49,10 +69,8 @@ const Footer = () => {
           </IconButton>
         </li>
         <li className="footer__item contrastbtn--hover click">
-          <IconButton className="icon__btn">
-            <a href="" className="icon__link">
-              <Contrast className="contrastbtn" />
-            </a>
+          <IconButton className="icon__btn" onClick={contrastOn}>
+            <Contrast className="contrastbtn" />
           </IconButton>
         </li>
       </ul>
