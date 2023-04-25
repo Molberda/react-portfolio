c8003803689b0bf8c7976a4fb1d31720c1ab5ac8
David Moller <davidmollera@hotmail.com>
2023-04-25 09:59:14 -0500

    added icon classes to the icons in the footer component

--- src/Items/Footer.jsx
+++ src/Items/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
               target="_blank"
               className="icon__link"
             >
-              <WhatsApp className="whabtn" />
+              <WhatsApp className="whabtn icon" />
             </a>
           </IconButton>
         </li>
@@ -35,7 +35,7 @@ const Footer = () => {
               target="_blank"
               className="icon__link"
             >
-              <Mail className="mailbtn" />
+              <Mail className="mailbtn icon" />
             </a>
           </IconButton>
         </li>
@@ -46,31 +46,31 @@ const Footer = () => {
               target="_blank"
               className="icon__link"
             >
-              <GitHub className="gitbtn" />
+              <GitHub className="gitbtn icon" />
             </a>
           </IconButton>
         </li>
         <li className="footer__item linkedbtn--hover click">
-          <IconButton className="icon__btn">
+          <IconButton className="icon__btn icon">
             <a
               href="https://www.linkedin.com/in/david-moller-2a9512255/"
               target="_blank"
               className="icon__link"
             >
-              <LinkedIn className="linkedbtn" />
+              <LinkedIn className="linkedbtn icon" />
             </a>
           </IconButton>
         </li>
         <li className="footer__item pdfbtn--hover click">
           <IconButton className="icon__btn">
             <a href="" className="icon__link">
-              <PictureAsPdf className="pdfbtn" />
+              <PictureAsPdf className="pdfbtn icon" />
             </a>
           </IconButton>
         </li>
         <li className="footer__item contrastbtn--hover click">
           <IconButton className="icon__btn" onClick={contrastOn}>
-            <Contrast className="contrastbtn" />
+            <Contrast className="contrastbtn icon" />
           </IconButton>
         </li>
       </ul>
