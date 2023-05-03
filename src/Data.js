73858d1efbee057e0b6e5fedc16c93a1c21ad970
David Moller <davidmollera@hotmail.com>
2023-05-03 17:49:26 -0500

    added css for the project info container

--- src/Data.js
+++ src/Data.js
@@ -3,7 +3,7 @@ export const projects = [
        id: 1,
        title: "Barco",
        img: "",
-       info: "info test",
+       info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque lobortis massa, et condimentum dolor congue in. Vivamus tortor diam, vestibulum in luctus quis, commodo ac lectus. Quisque placerat ipsum eu tellus venenatis, sed fermentum sem volutpat. In tempus diam at ultrices pellentesque. Maecenas eu pellentesque diam. Nam sit amet risus leo. Etiam eget orci at est feugiat dictum id ac quam.Vivamus libero nunc, ultrices a leo volutpat, pharetra vulputate augue. Donec varius urna eget risus tincidunt iaculis. Quisque posuere vehicula augue, in facilisis justo blandit at. Duis a dolor vel tortor accumsan volutpat. Etiam eget condimentum ligula. Cras ultrices augue sit amet diam dignissim, sit amet aliquam massa bibendum. Aliquam erat volutpat. Morbi finibus sem a rhoncus placerat.`,
        link: "test link" 
     },
     {
