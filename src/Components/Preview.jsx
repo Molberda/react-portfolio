48a5d6b0e5932825386116bdddcae049b651d201
David Moller <davidmollera@hotmail.com>
2023-05-05 16:03:09 -0500

    added html for the preview component

--- src/Components/Preview.jsx
+++ src/Components/Preview.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import { IconButton } from "@mui/material";
+import React from "react";
 
-const Preview = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+const Preview = ({ project }) => {
+  return (
+    <div className="preview__background">
+      <div className="background__info--container">
+        <h2>{project.title}</h2>
+        <a href={project.link}>Take a Look!</a>
+      </div>
+    </div>
+  );
+};
 
 export default Preview;
