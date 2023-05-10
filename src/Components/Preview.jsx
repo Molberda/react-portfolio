22c77954c84c945046d8a2acf2f3ae6a366145b4
David Moller <davidmollera@hotmail.com>
2023-05-10 12:04:52 -0500

    changed the preview btn type to a link type to make all of the btn clickable and function correctly

--- src/Components/Preview.jsx
+++ src/Components/Preview.jsx
@@ -6,8 +6,8 @@ const Preview = ({ project }) => {
     <div className="preview__background">
       <div className="background__info--container">
         <h2 className="preview__title">{project.title}</h2>
-        <Button variant='outlined' className="preview__btn">
-          <a href={project.link} className="preview__link">Take a Look!</a>
+        <Button variant='outlined' href={project.link} className="preview__btn">
+          <h3 className="preview__link">Take a Look!</h3>
         </Button>
       </div>
     </div>
