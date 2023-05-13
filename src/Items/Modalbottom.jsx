8e9338e5a79415d9ba62568d7a24ae79f8466b88
David Moller <davidmollera@hotmail.com>
2023-05-12 23:39:34 -0500

    created a form container

--- src/Items/Modalbottom.jsx
+++ src/Items/Modalbottom.jsx
@@ -10,25 +10,27 @@ const Modalbottom = () => {
           Dont hesitate to <span className="highlight">Contact Me!</span>
         </h1>
         <div className="modal__info--container">
-          <form action="" className="modal__form">
-            <div className="form__organizer">
-              <div className="form__item">
-                <label htmlFor="name">Name</label>
-                <input type="text" placeholder="John Doe" />
+          <div className="form__container">
+            <form action="" className="modal__form">
+              <div className="form__organizer">
+                <div className="form__item">
+                  <label htmlFor="name">Name</label>
+                  <input type="text" placeholder="John Doe" />
+                </div>
+                <div className="form__item">
+                  <label htmlFor="email">email</label>
+                  <input type="email" placeholder="johndoe@gmail.com" />
+                </div>
               </div>
               <div className="form__item">
-                <label htmlFor="email">email</label>
-                <input type="email" placeholder="johndoe@gmail.com" />
+                <label htmlFor="message">Message</label>
+                <textarea placeholder="I really like your work, lets have a chat!"></textarea>
               </div>
-            </div>
-            <div className="form__item">
-              <label htmlFor="message">Message</label>
-              <textarea placeholder="I really like your work, lets have a chat!"></textarea>
-            </div>
-            <Button type="submit" variant="contained" color="success">
-              <h1>Send!</h1>
-            </Button>
-          </form>
+              <Button type="submit" variant="contained" color="success">
+                <h1>Send!</h1>
+              </Button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
