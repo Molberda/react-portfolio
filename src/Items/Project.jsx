fe48d6374157b6543c200ffe71287306cc0e9c41
David Moller <davidmollera@hotmail.com>
2023-05-16 21:10:32 -0500

    added a use state fucntion and a use effect funtion to the project file aswell as a ternary operator to display the projects only if the images are loaded

--- src/Items/Project.jsx
+++ src/Items/Project.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Project = ({ item }) => {
+
+  const [img, setimg] = useState();
+
+  useEffect(() => {
+    const image = new Image()
+    image.src = item.img
+    image.onload = () => {
+      setTimeout(() => {
+        setimg(image)
+      }, 600)
+    }
+  });
+  
+  const image = new Image
+  
+
   return (
     <div className="project__container click">
-      <Link to={`/project/${item.id}`}>
+      {img ?
+       (<><Link to={`/project/${item.id}`}>
         <figure className="project__wrapper">
           <img
-            src={item.img}
+            src={img.src}
             alt=""
             className="project__img"
           />
@@ -15,7 +32,21 @@ const Project = ({ item }) => {
             <h1 className="project__title">{item.title}</h1>
           </span>
         </figure>
-      </Link>
+      </Link></>)
+       :
+        (<>  </>)}
+      {/* <Link to={`/project/${item.id}`}>
+        <figure className="project__wrapper">
+          <img
+            src={img}
+            alt=""
+            className="project__img"
+          />
+          <span className="project__background">
+            <h1 className="project__title">{item.title}</h1>
+          </span>
+        </figure>
+      </Link> */}
     </div>
   );
 };
