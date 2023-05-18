7ad4d11a974a670a10b8b1aef1bf8d7536ac5a60
David Moller <davidmollera@hotmail.com>
2023-05-18 17:19:12 -0500

    created an assets folder and imported barco img and used it in the data file

--- src/Data.js
+++ src/Data.js
@@ -1,10 +1,13 @@
+import Barco from './/Assets/Barco-landing.png'
+
+
 export const projects = [
   {
     id: 1,
     title: "Barco",
     info: `This is a website for a Colombian bar hence why its in spanish, it was designed and developed by me. It uses html, css, javascript and React. Some of the elements present on this project are the use of loading states for a better user experience, the items on the menu are renderized using a mock up API so its more dynamic and easier to add or remove new products, it also has working filters so the customers can filter out the type of product they want. Lastly it is fully optimized for desktops and smartphones.`,
     link: "https://barco-react-app.vercel.app",
-    img: "https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg",
+    img: Barco,
   },
   {
     id: 2,
