8b3630e16c10c1e7abc5c4c824410a06f0b5e605
David Moller <davidmollera@hotmail.com>
2023-05-18 19:12:24 -0500

    imported the deli img and updated the info for deli project in data file

--- src/Data.js
+++ src/Data.js
@@ -1,20 +1,21 @@
 import Barco from './/Assets/Barco-landing.png'
+import Deli from './Assets/Delizioso-landing.png'
 
 
 export const projects = [
   {
     id: 1,
     title: "Barco",
-    info: `This is a website for a Colombian bar hence why its in spanish, it was designed and developed by me. It uses html, css, javascript and React. Some of the elements present on this project are the use of loading states for a better user experience, the items on the menu are renderized using a mock up API so its more dynamic and easier to add or remove new products, it also has working filters so the customers can filter out the type of product they want. Lastly it is fully optimized for desktops and smartphones.`,
+    info: `This is a website for a Colombian bar hence why its in spanish, it was designed and developed by me. It uses HTML, CSS, JavaScript and React. Some of the elements present on this project are the use of loading states for a better user experience, the items on the menu are renderized using a mock up API so its more dynamic and easier to add or remove new products, it also has working filters so the customers can filter out the type of product they want. Lastly it is fully optimized for desktops and smartphones.`,
     link: "https://barco-react-app.vercel.app",
     img: Barco,
   },
   {
     id: 2,
     title: "Delizioso",
-    info: "info test",
-    link: "test link",
-    img: "https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg",
+    info: `This project was made for an entrepreneur chef looking to showcase her products and create a way for new customers to contact her. It's an original design and it uses HTML, CSS, JavaScript and React. It's a very simple website yet it acomplishes the goal that the client desired. `,
+    link: "https://jb-online-catalog.vercel.app",
+    img: Deli,
   },
   {
     id: 3,
