10f9321e6c47d9b453c2c4d53eb366f2826cf903
David Moller <davidmollera@hotmail.com>
2023-05-23 10:43:15 -0500

    imported the queries file to the app file and modified the width of the info container to use percentage so its responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import './Css/dark.css'
+import './Css/Queries.css'
 import Landing from "./Pages/Landing";
 import Projectinfo from "./Pages/Projectinfo";
 import { projects } from "./Data";
