2f61385157a9882ec1dfe6d6e0a3affbdb2e4749
David Moller <davidmollera@hotmail.com>
2023-05-23 16:11:54 -0500

    added padding to projects container and modified the postioning of the close btn in query

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import './Css/dark.css'
-import './Css/Queries.css'
 import Landing from "./Pages/Landing";
 import Projectinfo from "./Pages/Projectinfo";
 import { projects } from "./Data";
+import './Css/Queries.css'
 
 function App() {
   return (
