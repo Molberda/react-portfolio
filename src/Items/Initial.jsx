b80401013db79ffdb556faed6d6a876229991f4b
David Moller <davidmollera@hotmail.com>
2023-05-25 15:34:00 -0500

    added more css for the initial jsx file

--- src/Items/Initial.jsx
+++ src/Items/Initial.jsx
@@ -3,15 +3,15 @@ import React from "react";
 const Initial = () => {
   return (
     <div className="initial__wrapper">
-      <span className="initial 1">DM</span>
-      <span className="initial 2">DM</span>
-      <span className="initial 3">DM</span>
-      <span className="initial 4">DM</span>
-      <span className="initial 5">DM</span>
-      <span className="initial 6">DM</span>
-      <span className="initial 7">DM</span>
-      <span className="initial 8">DM</span>
-      <span className="initial 9">DM</span>
+      <span className="initial i1">DM</span>
+      <span className="initial i2">DM</span>
+      <span className="initial i3">DM</span>
+      <span className="initial i4">DM</span>
+      <span className="initial i5">DM</span>
+      <span className="initial i6">DM</span>
+      <span className="initial i7">DM</span>
+      <span className="initial i8">DM</span>
+      <span className="initial i9">DM</span>
     </div>
   );
 };
