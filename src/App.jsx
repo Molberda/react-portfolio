2f1ea8ac5ea12cb5ab551d15208a71d51cb115c1
David Moller <davidmollera@hotmail.com>
2023-05-25 14:58:39 -0500

    imported Initial component to the app jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import './Css/dark.css'
 import Landing from "./Pages/Landing";
 import Projectinfo from "./Pages/Projectinfo";
 import { projects } from "./Data";
+import Initial from "./Items/Initial";
 import './Css/Queries.css'
 
 function App() {
   return (
     <Router>
+      <Initial/>
       <Routes>
         <Route path="/" element={<Landing/>}/>
         <Route path="/project/:id" element={<Projectinfo projects={projects}/>}/>
