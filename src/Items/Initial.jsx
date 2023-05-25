381d3fcd21986ea1550ac09ba879f284a4da6029
David Moller <davidmollera@hotmail.com>
2023-05-25 15:23:36 -0500

    added more html for the initial jsx and used position absolute for the initial

--- src/Items/Initial.jsx
+++ src/Items/Initial.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Initial = () => {
-  return <span className="initial">DM</span>;
+  return (
+    <div className="initial__wrapper">
+      <span className="initial 1">DM</span>
+      <span className="initial 2">DM</span>
+      <span className="initial 3">DM</span>
+      <span className="initial 4">DM</span>
+      <span className="initial 5">DM</span>
+      <span className="initial 6">DM</span>
+      <span className="initial 7">DM</span>
+      <span className="initial 8">DM</span>
+      <span className="initial 9">DM</span>
+    </div>
+  );
 };
 
 export default Initial;
