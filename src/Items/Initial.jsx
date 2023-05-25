e75983be1cf4a14298f40b72a68e2a9465470d9a
David Moller <davidmollera@hotmail.com>
2023-05-25 15:10:36 -0500

    added initial css for the initial component

--- src/Items/Initial.jsx
+++ src/Items/Initial.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Initial = () => {
-  return (
-    <span className="initial__wrapper">
-      <h1 className="initial">DM</h1>
-    </span>
-  );
+  return <span className="initial">DM</span>;
 };
 
 export default Initial;
